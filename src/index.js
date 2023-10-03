c8b0d737afce89a41cf02d81cb09fd18ac91c8d3
jimmycanosa <44603136+jimmycanosa@users.noreply.github.com>
2023-10-03 16:12:00 +0800

    Writing markup with JSX

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './styles.css';
+import App from './App.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -15,3 +15,26 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// import { createRoot } from 'react-dom/client';
+// import App from './App';
+
+// const root = createRoot(document.getElementById('root'));
+// root.render(
+//   <App />
+// );
+
+// import { createRoot } from 'react-dom/client';
+
+// const root = createRoot(document.getElementById('root'));
+// root.render(<h1>Hello, world</h1>);
+
+// import { createRoot } from 'react-dom/client';
+
+// function NavigationBar() {
+//   return <h1>Hello from React</h1>;
+// }
+
+// const domNode = document.getElementById('navigation');
+// const root = createRoot(domNode);
+// root.render(<NavigationBar />);
