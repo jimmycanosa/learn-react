d53124b27854357621428c1ededd438076eea1dd
jimmycanosa <44603136+jimmycanosa@users.noreply.github.com>
2023-10-03 17:16:33 +0800

    JavaScript in JSX w/ curly braces challenge 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+
+const baseUrl = 'https://i.imgur.com/';
 const person = {
   name: 'Gregorio Y. Zara',
-  avatar: "https://i.imgur.com/7vQD0fPs.jpg",
+  imageId: '7vQD0fP',
+  imageSize: 's',
   theme: {
     backgroundColor: 'black',
     color: 'pink'
@@ -13,8 +16,8 @@ export default function TodoList() {
       <h1>{person.name}'s Todos</h1>
       <img
         className="avatar"
-        src={person.avatar}
-        alt="Gregorio Y. Zara"
+        src={baseUrl + person.imageId + person.imageSize + '.jpg'}
+        alt={person.name}
       />
       <ul>
         <li>Improve the videophone</li>
