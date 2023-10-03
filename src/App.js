ee0b064b29220b3e29885735785842a2c670978d
jimmycanosa <44603136+jimmycanosa@users.noreply.github.com>
2023-10-03 20:54:13 +0800

    Passing props to a component challenge 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import { getImageUrl } from './utils.js';
-
-function Avatar({ person, size }) {
+function Card({ children }) {
   return (
-    <img
-      className='avatar'
-      src={getImageUrl(person, size > 90 ? 'b' : 's')}
-      alt={person.name}
-      width={size}
-      height={size}
-    />
+    <div className='card'>
+      <div className='card-content'>{children}</div>
+    </div>
   );
 }
 
 export default function Profile() {
   return (
-    <Avatar
-      size={80}
-      person={{
-        name: 'Gregorio Y. Zara',
-        imageId: '7vQD0fP',
-      }}
-    />
+    <div>
+      <Card>
+        <h1>Photo</h1>
+        <img
+          className='avatar'
+          src='https://i.imgur.com/OKS67lhm.jpg'
+          alt='Aklilu Lemma'
+          width={70}
+          height={70}
+        />
+      </Card>
+      <Card>
+        <h1>About</h1>
+        <p>
+          Aklilu Lemma was a distinguished Ethiopian scientist who discovered a
+          natural treatment to schistosomiasis.
+        </p>
+      </Card>
+    </div>
   );
 }
