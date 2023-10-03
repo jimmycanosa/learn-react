d93668ab966261d18e787d56e558e0487b81594b
jimmycanosa <44603136+jimmycanosa@users.noreply.github.com>
2023-10-03 21:49:51 +0800

    Conditional rendering challenge 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-function Item({ name, isPacked }) {
+function Item({ name, importance }) {
   return (
-    <li className='item'>
-      {name} {isPacked ? '✔' : '❌'}
+    <li className="item">
+      {name} {importance > 0 && <i>(Importance: {importance})</i>}
     </li>
   );
 }
@@ -11,9 +11,18 @@ export default function PackingList() {
     <section>
       <h1>Sally Ride's Packing List</h1>
       <ul>
-        <Item isPacked={true} name='Space suit' />
-        <Item isPacked={true} name='Helmet with a golden leaf' />
-        <Item isPacked={false} name='Photo of Tam' />
+        <Item 
+          importance={9} 
+          name="Space suit" 
+        />
+        <Item 
+          importance={0} 
+          name="Helmet with a golden leaf" 
+        />
+        <Item 
+          importance={6} 
+          name="Photo of Tam" 
+        />
       </ul>
     </section>
   );
