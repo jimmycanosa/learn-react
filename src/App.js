e77f3c8effdd4b86f527162f3b5662a1c321d227
jimmycanosa <44603136+jimmycanosa@users.noreply.github.com>
2023-10-04 08:42:04 +0800

    Conditional rendering challenge 3b

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-function Drink({ name }) {
-  let part, caffeine, age;
-  if (name === 'tea') {
-    part = 'leaf';
-    caffeine = '15–70 mg/cup';
-    age = '4,000+ years';
-  } else if (name === 'coffee') {
-    part = 'bean';
-    caffeine = '80–185 mg/cup';
-    age = '1,000+ years';
+const drinks = {
+  tea: {
+    part: 'leaf',
+    caffeine: '15–70 mg/cup',
+    age: '4,000+ years'
+  },
+  coffee: {
+    part: 'bean',
+    caffeine: '80–185 mg/cup',
+    age: '1,000+ years'
   }
+};
+
+function Drink({ name }) {
+  const info = drinks[name];
   return (
     <section>
       <h1>{name}</h1>
       <dl>
         <dt>Part of plant</dt>
-        <dd>{part}</dd>
+        <dd>{info.part}</dd>
         <dt>Caffeine content</dt>
-        <dd>{caffeine}</dd>
+        <dd>{info.caffeine}</dd>
         <dt>Age</dt>
-        <dd>{age}</dd>
+        <dd>{info.age}</dd>
       </dl>
     </section>
   );
@@ -27,8 +31,8 @@ function Drink({ name }) {
 export default function DrinkList() {
   return (
     <div>
-      <Drink name='tea' />
-      <Drink name='coffee' />
+      <Drink name="tea" />
+      <Drink name="coffee" />
     </div>
   );
 }
