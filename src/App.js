c190d7a5000748b81558c76acda73a22aaa3bfb8
jimmycanosa <44603136+jimmycanosa@users.noreply.github.com>
2023-10-04 17:14:57 +0800

    Rendering lists challenge 3a

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import { recipes } from './data.js';
 
-function RecipeSection({ recipes }) {
-  return recipes.map((recipe) => (
-    <div key={recipe.id}>
-      <h2>{recipe.name}</h2>
+function Recipe({ id, name, ingredients }) {
+  return (
+    <div>
+      <h2>{name}</h2>
       <ul>
-        {recipe.ingredients.map((ingredient) => (
+        {ingredients.map((ingredient) => (
           <li key={ingredient}>{ingredient}</li>
         ))}
       </ul>
     </div>
-  ));
+  );
 }
 
 export default function RecipeList() {
   return (
     <div>
       <h1>Recipes</h1>
-      <RecipeSection recipes={recipes} />;
+      {recipes.map((recipe) => (
+        <Recipe
+          id={recipe.id}
+          name={recipe.name}
+          ingredients={recipe.ingredients}
+          key={recipe.id}
+        />
+      ))}
     </div>
   );
 }
