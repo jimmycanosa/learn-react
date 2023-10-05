d38bfeb527e827d19db090480968feaf4f2d550b
jimmycanosa <44603136+jimmycanosa@users.noreply.github.com>
2023-10-05 16:31:43 +0800

    Keeping components pure challenge 3b

--- src/StoryTray.js
+++ src/StoryTray.js
@@ -1,12 +1,20 @@
 export default function StoryTray({ stories }) {
+  // Copy the array!
+  let storiesToDisplay = stories.slice();
+
+  // Does not affect the original array:
+  storiesToDisplay.push({
+    id: 'create',
+    label: 'Create Story'
+  });
+
   return (
     <ul>
-      {stories.map(story => (
+      {storiesToDisplay.map(story => (
         <li key={story.id}>
           {story.label}
         </li>
       ))}
-      <li>Create Story</li>
     </ul>
   );
 }
